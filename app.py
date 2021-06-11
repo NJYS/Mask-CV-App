cc5c234c0ccf98a0520e835dffbf5038043e0464
Park Kyeong Hwan <john175258@gmail.com>
2021-06-11 17:25:17 +0900

    cors allow all 추가

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, send_file  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
 from run_model import run_model
+from flask_cors import CORS
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
-
+CORS(app)
 
 lists = {}
 count = 1
